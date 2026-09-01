2ee890ef30ab7809bb213d90a6579b0d3ddd63e2
st26233-hash <st26233@ormiston.school.nz>
2026-09-01 23:02:00 +0000

    home -> menu journey

--- app.py
+++ app.py
@@ -10,6 +10,20 @@ def home():
     """Display the home page."""
     return render_template("index.html")
 
+# Displays the menu when navigating to /menu
+
+@app.route("/menu")
+def menu():
+    """Display the pizza menu."""
+    return render_template("menu.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+# displaying menu.html at menu page
+
+@app.route("/menu")
+def menu():
+    """Display the pizza menu."""
+    return render_template("menu.html")
