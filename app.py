970724fec62f300c7966b52e88372c08c49a850e
st26233-hash <st26233@ormiston.school.nz>
2026-09-01 23:20:31 +0000

    customising pizza options

--- app.py
+++ app.py
@@ -4,6 +4,40 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+# collection of pizza data
+
+pizzas = [
+    {
+        "id": 1,
+        "name": "Margherita",
+        "description": "Tomato sauce, mozzarella and basil",
+        "price": 14.00
+    },
+    {
+        "id": 2,
+        "name": "Pepperoni",
+        "description": "Tomato sauce, mozzarella and pepperoni",
+        "price": 16.00
+    },
+    {
+        "id": 3,
+        "name": "Meat Lovers",
+        "description": "Tomato sauce, mozzarella, beef, ham and pepperoni",
+        "price": 19.00
+    },
+    {
+        "id": 3,
+        "name": "Vegetarian",
+        "description": "Tomato sauce, mozzarella, mushrooms, capsicum and onion",
+        "price": 17.00
+    },
+    {
+        "id": 4,
+        "name": "BBQ Chicken",
+        "description": "BBQ sauce, mozzarella, chicken and onion",
+        "price": 19.00
+    }
+]
 
 @app.route("/")
 def home():
@@ -15,7 +49,7 @@ def home():
 @app.route("/menu")
 def menu():
     """Display the pizza menu."""
-    return render_template("menu.html")
+    return render_template("menu.html", pizzas=pizzas)
 
 
 if __name__ == "__main__":
@@ -27,3 +61,25 @@ if __name__ == "__main__":
 def menu():
     """Display the pizza menu."""
     return render_template("menu.html")
+
+# customisation form route
+
+ @app.route("/customise/<int:pizza_id>", methods=["GET", "POST"])
+def customise(pizza_id):
+    """Display and process pizza customisation."""
+
+    for pizza in pizzas:
+        if pizza["id"] == pizza_id:
+
+            if request.method == "POST":
+                size = request.form["size"]
+                toppings = request.form.getlist("topping")
+                quantity = request.form["quantity"]
+
+                print(size)
+                print(toppings)
+                print(quantity)
+
+            return render_template("customise.html", pizza=pizza)
+
+    return "Pizza not found", 404
